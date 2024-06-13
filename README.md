0b2f84648f234415bfd371d1566e4f7d925b469e
Ikay <divinevictoryihemegbulem@mail.com>
2024-06-13 01:30:47 +0100

    Addition of Joi validation to maintain data integrity, modified ReadMe to handle proper documentation and usage

--- README.md
+++ README.md
@@ -1,1 +1,167 @@
-# school_management_system
+# Mone-X school_management_system
+
+# Overview
+
+The Monex-X school_management_system (SMS) is a comprehensive solution designed to streamline the management of academic institutions. It includes features for handling Admin Student and Teacher profiles, courses, exams, results, and more. The system is built using Node.js, Express.js, and MongoDB, providing a robust and scalable backend.
+
+# Table of Contents
+Installation
+Configuration
+Usage
+API Endpoints
+Database Models
+Validation
+Contributing
+License
+Installation
+
+# Clone the repository:
+bash
+git clone https://github.com/Ikay14/mone-X_school_mgt_system.git
+
+# Navigate to the project directory:
+bash
+cd mone-X_school_mgt_system
+
+# Install dependencies:
+bash
+npm install
+
+# Set up environment variables:
+Create a .env file in the root directory and add the following variables:
+
+# makefile
+MONGO_URI=your_mongodb_connection_string
+JWT_SECRET=your_jwt_secret
+JWT_LIFETIME=your_jwt_lifetime 
+PORT=your_port_number
+
+# Start the server:
+bash
+npm start
+
+# Configuration
+Ensure you have MongoDB installed and running. Update the MONGO_URI in the .env file with your MongoDB connection string.
+
+# Usage
+Once the server is running, you can interact with the API using tools like Postman or by integrating it with your frontend application. The following sections outline the available API endpoints and their usage.
+
+# API Endpoints
+
+# Authentication
+Register Admin
+POST /api/admin/register
+
+Login
+POST /api/admin/login
+
+Admin Profile
+GET /api/admin/profile
+
+Update Admin Profile
+PATCH /api/admin/update-profile
+
+# Students
+Create Student
+POST /api/student/admin/register-student
+
+Login Student
+POST /api/student/login-student
+
+Get Student Profile
+GET /api/student/profile
+
+Get All Students (Admin)
+GET /api/student/admin/view-students
+
+Get Student By Admin
+GET /api/student/:studentId/admin
+
+Admin Update Student Info
+PATCH /api/student/:studentId/update-student/admin
+
+Update Student Profile
+PATCH /api/student/update-profile
+
+Student Write Exam
+PATCH /api/student/write-exam
+
+# Teachers
+Create Teacher
+POST /api/teacher/create-teacher
+
+Login Teacher
+POST /api/teacher/login-teacher
+
+Get Teacher Profile
+GET /api/teacher/profile
+
+Get All Teachers
+GET /api/teacher/view-teachers
+
+Update Teacher Profile
+PATCH /api/teacher/update-profile
+
+Update Teacher Profile
+PUT /api/teacher/update-profile
+
+# Database Models
+## Academic Model
+AcademicYear
+Course
+Department
+Exam
+Level
+Question
+Result
+Semester
+
+## Staff Model
+Admin
+Teacher
+
+## Student Model
+Student
+
+## Example Schema
+
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  classLevel: String,
+  examResults: [{ type: ObjectId, ref: 'Result' }],
+}, { timestamps: true });
+
+
+# Validation
+Validation is handled using Joi. Example validation schema for creating a new student:
+
+## require the package
+const Joi = require('joi');
+
+const validateStudent = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    classLevel: Joi.string().required(),
+  });
+ 
+
+# Contributing
+Contributions are welcome! Please fork the repository and create a pull request with your changes.
+
+# Fork the repository
+Create your feature branch (git checkout -b feature/branch-name)
+Commit your changes (git commit -am 'Add some feature')
+Push to the branch (git push origin feature/branch-name)
+Create a new Pull Request
+
+# License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+
+
--- middlewares/Academic/academic.validator.js
+++ middlewares/Academic/academic.validator.js
@@ -1,6 +1,5 @@
 const Joi = require('joi')
 
-
 const validateAcademicYear = Joi.object ({
         name: Joi.string().required(),
         fromYear: Joi.date().required(),
@@ -9,7 +8,7 @@ const validateAcademicYear = Joi.object ({
         createdBy: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
         students: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)),
         teachers: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/))
-    });
+});
 
 const validateCourse = Joi.object({
         name: Joi.string().required(),
@@ -19,7 +18,7 @@ const validateCourse = Joi.object({
         semester: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
         createdBy: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
         duration: Joi.string().required().default('2 hours'),
-    });
+});
     
 const validateDepartment =  Joi.object({
             name: Joi.string().required(),
@@ -29,12 +28,81 @@ const validateDepartment =  Joi.object({
             teachers: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)), // Array of ObjectId strings
             students: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)), // Array of ObjectId strings
             course: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)) // Array of ObjectId strings
-        });    
+});   
 
-    module.exports = 
+const validateExam = Joi.object({
+              name: Joi.string().required(),
+              description: Joi.string().required(),
+              course: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+              department: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+              passMark: Joi.number().required().default(50),
+              totalMark: Joi.number().required().default(100),
+              duration: Joi.string().required().default('30 minutes'),
+              examDate: Joi.date().required().default(() => new Date(), 'current date'),
+              examTime: Joi.string().required(),
+              examType: Joi.string().required().default('Quiz'),
+              examStatus: Joi.string().required().valid('pending', 'live').default('pending'),
+              questions: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)), // Array of ObjectId strings
+              level: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+              createdBy: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+              semester: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+              academicYear: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+ });  
+
+const validateLevel = Joi.object({
+             name: Joi.string().required(),
+             description: Joi.string().optional(),
+             createdBy: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+             students: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).optional(), // Array of ObjectId strings
+             subjects: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).optional(), // Array of ObjectId strings
+             teachers: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).optional(), // Array of ObjectId strings
+});
+
+const validateQuestion = Joi.object({
+              question: Joi.string().required(),
+              optionA: Joi.string().required(),
+              optionB: Joi.string().required(),
+              optionC: Joi.string().required(),
+              optionD: Joi.string().required(),
+              correctAnswer: Joi.string().required(),
+              isCorrect: Joi.boolean().default(false),
+              createdBy: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+});
+
+const validateExamResult = Joi.object({
+              student: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+              exam: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+              grade: Joi.number().required(),
+              score: Joi.number().required(),
+              passMark: Joi.number().required().default(50),
+              answeredQuestions: Joi.array().items(Joi.object()).required(),
+              status: Joi.string().valid("failed", "passed").required().default("failed"),
+              remarks: Joi.string().valid("Excellent", "Good", "Poor").required().default("Poor"),
+              teacher: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+              course: Joi.string().regex(/^[0-9a-fA-F]{24}$/).optional(),
+              level: Joi.string().regex(/^[0-9a-fA-F]{24}$/).optional(),
+              semester: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+              academicYear: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+              isPublished: Joi.boolean().default(false),
+});
+
+const validateSemester  = Joi.object({
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      duration: Joi.string().required().default('4 months'),
+      createdBy: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(), // Validate ObjectId as a string
+    });
+                     
+
+module.exports = 
     { 
         validateAcademicYear,
         validateCourse,
-        validateDepartment
+        validateDepartment,
+        validateExam,
+        validateLevel,
+        validateQuestion,
+        validateExamResult,
+        validateSemester
     }
   
